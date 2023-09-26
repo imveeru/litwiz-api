38ea27ad51899467d0eb6f31c9874e28e2c6ca80
imvee <vind.veeru@gmail.com>
2023-09-26 21:50:32 +0530

    AWS Lambda

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from ai_functions import get_lit_survey,get_abstract,get_title
+from mangum import Mangum
 
 app = FastAPI()
+handler=Mangum(app)
 
 @app.get("/")
 async def root():
